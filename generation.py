ef8202fed062c5b20aab8556464166d4344acece
Hunter McGivern <huntermsm2002@gmail.com>
2024-10-13 03:02:10 -0400

    updated some ugly code

--- generation.py
+++ generation.py
@@ -20,52 +20,68 @@ def amountOfSystemFeatures():
 #generates the system feature
 def systemFeatures():
     
-    systemfeature = random.randint(1, 10)
-    feature = ""
+    systemFeature = random.randint(1, 10) # random int 1-10 assigned to systemFeature
 
-    if systemfeature == 1:
-        feature = "Bountiful"
-    elif systemfeature == 2:
-        feature = "Gravity_Tides"
-    elif systemfeature == 3:
-        feature = "Haven"
-    elif systemfeature == 4:
-        feature = "Ill_Omened"
-    elif systemfeature == 5:
-        feature = "Pirate_Den"
-    elif systemfeature == 6:
-        feature = "Ruined_Empire"
-    elif systemfeature == 7:
-        feature = "Starfarers"
-    elif systemfeature == 8:
-        feature = "Stellar_Anomoly"
-    elif systemfeature == 9:
-        feature = "Warp_Stasis"
-    elif systemfeature == 10:
-        feature = "Warp_Turbulence"
-    return feature
+    features = {      # dictonary of features based on roll
+       1: "Bountiful",
+       2: "Gravity_Tides",
+       3: "Haven",
+       4: "Ill_Omened",
+       5: "Pirate_Den",
+       6: "Ruined_Empire",
+       7: "Starfarers",
+       8: "Stellar_Anomoly",
+       9: "Warp_Stasis",
+       10: "Warp_Turbulence"
+   }
+    #return feature based on roll
+    return features[systemFeature]
+
+
+
+    #UGLY WAY OF DOING IT GO AWAY
+    #if systemfeature == 1:
+     #  feature = "Bountiful"
+  #  elif systemfeature == 2:
+   #     feature = "Gravity_Tides"
+  #  elif systemfeature == 3:
+  #      feature = "Haven"
+   # elif systemfeature == 4:
+   #     feature = "Ill_Omened"
+   # elif systemfeature == 5:
+   #     feature = "Pirate_Den"
+   # elif systemfeature == 6:
+   #     feature = "Ruined_Empire"
+   # elif systemfeature == 7:
+   #     feature = "Starfarers"
+   # elif systemfeature == 8:
+    #    feature = "Stellar_Anomoly"
+    #elif systemfeature == 9:
+   #     feature = "Warp_Stasis"
+   # elif systemfeature == 10:
+       # feature = "Warp_Turbulence"
+    #return feature
+
 
 #generates the star type
 def starType():
 
-    star = random.randint(1,10)
-    type = ""
+    star = random.randint(1,10) # random int 1-10 assigned to star
 
-    if star == 1:
-        type = "Mighty"
-    elif 2 <=  star <= 4:
-        type = "Vigorous"
-    elif 5 <= star <= 7:
-        type = "Luminous"
-    elif star == 8:
-        type = "Dull"
-    elif star == 9:
-        type = "Anomalous"
-    elif star == 10:
-        type = "Binary"
-    
-    return type
-
+    starMap = {     # dictonary of star types based on roll   
+        1: "Mighty",
+        2: "Vigorous",
+        3: "Vigorous",         
+        4: "Vigorous",
+        5: "Luminous",
+        6: "Luminous",
+        7: "Luminous",
+        8: "Dull",
+        9: "Anomalous",
+        10: "Binary"
+    }
+    #return star type based on roll
+    return starMap[star]
 
 
     #DEBUGGING
